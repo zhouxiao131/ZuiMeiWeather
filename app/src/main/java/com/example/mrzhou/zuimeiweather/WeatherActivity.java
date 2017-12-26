10e89af0966bae675d3108c9377f5678ad54277e
zhou <z1312431054@outlook.com>
2017-12-26 15:10:06 +0800

    2017年12月26日15:10:01

--- app/src/main/java/com/example/mrzhou/zuimeiweather/WeatherActivity.java
+++ app/src/main/java/com/example/mrzhou/zuimeiweather/WeatherActivity.java
@@ -135,8 +135,9 @@ public class WeatherActivity extends AppCompatActivity {
      * 根据天气id请求城市天气信息。
      */
     public void requestWeather(final String weatherId) {
-        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
-        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+       String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=c3dba800215443b1b470aeedd466e45e";
+        //String weatherUrl ="https://free-api.heweather.com/s6/weather/forecast?location=潍坊&key=c3dba800215443b1b470aeedd466e45e";
+                HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
